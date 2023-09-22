6478977b75319c8e413552c25171b767992f13a9
verners <vernersg@gmail.com>
2023-09-22 14:53:01 +0300

    added ampty project

---
+++ src/main/java/com/example/ldc/LdcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ldc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ldc/LdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ldc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
